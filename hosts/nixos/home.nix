9e1465fa01ea0343aeacc45c02670df5f4497435
Ernest Gupik <ernestgupik@wp.pl>
2024-05-09 14:14:41 +0200

    97 current  2024-05-09 14:14:34  24.05.20240507.b211b39  6.6.30                          *

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -169,6 +169,7 @@ export PS1="\[$(tput bold)\]\[\033[38;5;0m\]\[\033[48;5;11m\]\u\[$(tput sgr0)\] 
 set -g status-style 'bg=#282828'
 setw -g window-status-current-style 'fg=black,bg=gray'
     '';
+    customPaneNavigationAndResize = true;
     #tmuxConf = ''
 #set -g mouse on
 ## set -g status-position top
