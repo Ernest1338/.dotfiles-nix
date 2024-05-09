93977d3806eb3c9d9c38c113a670274f16c33ea3
Ernest Gupik <ernestgupik@wp.pl>
2024-05-09 15:59:36 +0200

    129 current  2024-05-09 15:59:27  24.05.20240507.b211b39  6.6.30                          *

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -16,7 +16,7 @@
       "monitor" = ",preferred,auto,auto";
       general = {
         border_size = 1;
-        gaps_in = 4;
+        gaps_in = 2;
         gaps_out = 4;
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
