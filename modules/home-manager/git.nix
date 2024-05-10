97ff4993317f5ceb7faeaa2122aff7f4383c6f1a
Ernest Gupik <ernestgupik@wp.pl>
2024-05-10 08:25:02 +0200

    13 current  2024-05-10 08:24:23  24.05.20240507.b211b39   6.6.30                          *

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,6 +1,8 @@
 { lib, ... }: {
   programs.git = {
     enable = true;
+    userName = "Ernest Gupik";
+    userEmail = "ernestgupik@wp.pl";
     extraConfig = {
       credential.helper = "store";
     };
