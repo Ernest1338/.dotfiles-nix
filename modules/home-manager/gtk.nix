f03fc1c64f5c3e81368e3cee7cbb77713c1b77bd
Ernest Gupik <ernestgupik@wp.pl>
2024-05-11 11:39:21 +0200

    252 current  2024-05-11 11:39:03  24.05.20240509.f1010e0  6.6.30                          *

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -5,6 +5,14 @@
             name = "orchis-theme";
             package = pkgs.orchis-theme;
         };
+        iconTheme = {
+            name = "Adwaita";
+            package = pkgs.gnome.adwaita-icon-theme;
+        };
+        #cursorTheme = {
+        #    name = "Adwaita";
+        #    package = pkgs.gnome.adwaita-icon-theme;
+        #};
         gtk2.extraConfig = "gtk-application-prefer-dark-theme = 1";
         gtk3.extraConfig = {
             gtk-application-prefer-dark-theme = true;
