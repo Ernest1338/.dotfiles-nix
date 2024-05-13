27ef7a7ed7ea8b4dea8f37999ed80857c4f26983
Ernest Gupik <ernestgupik@wp.pl>
2024-05-13 19:28:07 +0200

    467 current  2024-05-13 19:27:09  24.05.20240512.2057814  6.6.30                          *

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -2,5 +2,7 @@
     home.packages = with pkgs; [ mako ];
     services.mako = {
         enable = true;
+        backgroundColor = "#2b3035ff";
+        textColor = "#ebdbb2ff";
     };
 }
--- rebuild.sh
+++ rebuild.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
 pushd ~/.dotfiles/ &>/dev/null
+
 git diff
 echo "NixOS Rebuilding..."
+
 git pull
 git add .
+
 hname=$(hostname)
 sudo nixos-rebuild switch --cores $(nproc) --flake .#$hname || exit 1
-gen=$(nixos-rebuild list-generations | grep current)
+
 read -p "Commit? (y) " -n 1 -r
 echo
 if [[ $REPLY = "" || $REPLY =~ ^[Yy]$ ]]; then
+    gen=$(nixos-rebuild list-generations | grep current)
     git commit -am "$gen"
 fi
+
 read -p "Push? (n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     git push
 fi
+
 popd &>/dev/null
