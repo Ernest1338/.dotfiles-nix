86a0d02a3f557fa81a40f0e1fd120724f051635f
Ernest Gupik <ernestgupik@wp.pl>
2024-05-15 00:13:33 +0200

    528 current  2024-05-15 00:13:22  24.05.20240512.2057814  6.6.30                          *

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -41,16 +41,16 @@
             };
             animations = {
                 enabled = true;
-                bezier = [
-                    "myBezier, 0.65, 0, 0.35, 1"
-                ];
+                # bezier = [
+                #     "myBezier, 0.65, 0, 0.35, 1"
+                # ];
                 animation = [
-                    #"windows, 1, 7, myBezier"
-                    "windowsOut, 1, 7, default, popin 80%"
+                    "windows, 1, 8, default, popin 80%"
+                    #"windowsOut, 1, 8, default, popin 80%"
                     "border, 1, 10, default"
                     "borderangle, 1, 8, default"
                     "fade, 1, 7, default"
-                    "workspaces, 1, 6, default"
+                    "workspaces, 1, 6, default, slidefade 20%"
                 ];
             };
             dwindle = {
