bc4738b371b71cc4d16a79a5bcddc9e1a9b750cb
Ernest Gupik <ernestgupik@wp.pl>
2024-05-21 00:46:23 +0200

    18 current  2024-05-21 00:45:17  24.05.20240519.6c0b7a9  6.6.31                          *

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,13 +1,15 @@
 { lib, ... }: {
-    # boot.loader = {
-    #     systemd-boot.enable = false;
-    #     grub = {
-    #         enable = true;
-    #         devices = [ "nodev" ];
-    #         useOSProber = false;
-    #     };
-    #     efi.canTouchEfiVariables = true;
-    # };
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader = {
+        systemd-boot.enable = false;
+        grub = {
+            enable = true;
+            copyKernels = true;
+            efiInstallAsRemovable = true;
+            efiSupport = true;
+            devices = [ "nodev" ];
+            useOSProber = false;
+        };
+    };
+    # boot.loader.systemd-boot.enable = true;
+    # boot.loader.efi.canTouchEfiVariables = true;
 }
