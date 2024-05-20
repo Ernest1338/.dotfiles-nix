c6204a7446fb74bb027fc48fa1c7c89652bcdf46
Ernest Gupik <ernestgupik@wp.pl>
2024-05-21 01:03:31 +0200

    Switch to systemd-boot

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,15 +1,31 @@
 { lib, ... }: {
-    boot.loader = {
-        systemd-boot.enable = false;
-        grub = {
-            enable = true;
-            copyKernels = true;
-            efiInstallAsRemovable = true;
-            efiSupport = true;
-            devices = [ "nodev" ];
-            useOSProber = false;
-        };
-    };
-    # boot.loader.systemd-boot.enable = true;
-    # boot.loader.efi.canTouchEfiVariables = true;
+    # Grub UEFI
+    # boot.loader = {
+    #     systemd-boot.enable = false;
+    #     grub = {
+    #         enable = true;
+    #         copyKernels = true;
+    #         efiInstallAsRemovable = true;
+    #         efiSupport = true;
+    #         devices = [ "nodev" ];
+    #         useOSProber = false;
+    #     };
+    # };
+
+    # Grub BIOS
+    # boot.loader = {
+    #     systemd-boot.enable = false;
+    #     grub = {
+    #         enable = true;
+    #         copyKernels = true;
+    #         efiInstallAsRemovable = true;
+    #         efiSupport = true;
+    #         devices = [ "nodev" ];
+    #         useOSProber = false;
+    #     };
+    # };
+
+    # Systemd-boot UEFI
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 }
