e2c37dfbc159b87c5b6024d62fb3f05c588156ff
Ernest Gupik <ernestgupik@wp.pl>
2024-05-21 13:50:59 +0200

    33 current  2024-05-21 13:49:53  24.05.20240519.6c0b7a9  6.6.31                          *

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -14,7 +14,7 @@
         ../../modules/home-manager/mako.nix
         ../../modules/home-manager/fuzzel.nix
         ../../modules/home-manager/hyprlock.nix
-        ../../modules/home-manager/udiskie.nix
+        # ../../modules/home-manager/udiskie.nix
 
         ../../modules/home-manager/hyprland.nix
         ../../modules/home-manager/waybar.nix
