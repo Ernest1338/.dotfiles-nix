1300817bf68b5ff871b07404fa421d8acfa227fd
Ernest Gupik <ernestgupik@wp.pl>
2024-05-21 12:14:45 +0200

    22 current  2024-05-21 12:12:49  24.05.20240519.6c0b7a9   6.6.31                          *

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -22,17 +22,4 @@
             # STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
         };
     };
-    services.auto-cpufreq = {
-        enable = true;
-        settings = {
-            battery = {
-                governor = "powersave";
-                turbo = "never";
-            };
-            charger = {
-                governor = "performance";
-                turbo = "auto";
-            };
-        };
-    };
 }
