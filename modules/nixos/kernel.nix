7524c3c2e65d111f8bb0299957e1203d65a1ff8b
Ernest Gupik <ernestgupik@wp.pl>
2024-05-21 19:40:13 +0200

    77 current  2024-05-21 19:39:05  24.05.20240519.6c0b7a9  6.6.31                          *

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -1,4 +1,6 @@
 { lib, ... }: {
     # i2c required for controlling display brightness
     hardware.i2c.enable = true;
+    # support for AMD gpu
+    boot.initrd.kernelModules = [ "amdgpu" ];
 }
--- modules/nixos/opengl.nix
+++ modules/nixos/opengl.nix
@@ -1,4 +1,7 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, vars, ... }: {
     hardware.opengl.enable = true;
+    hardware.opengl.driSupport = true;
     hardware.opengl.driSupport32Bit = true;
+    # for laptop gpu
+    boot.kernelParams = [ "radeon.si_support=0" "amdgpu.si_support=1" ];
 }
