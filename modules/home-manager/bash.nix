33aeead5816712b5e09f2d975ac9840c56a441a9
Ernest Gupik <ernestgupik@wp.pl>
2024-05-21 15:56:32 +0200

    61 current  2024-05-21 15:56:13  24.05.20240519.6c0b7a9  6.6.31                          *

--- modules/home-manager/bash.nix
+++ modules/home-manager/bash.nix
@@ -10,7 +10,7 @@
             "gc" = "sudo nix-collect-garbage -d && sudo nix-store --gc && sudo nix-env --delete-generations old";
             "up" = "~/.dotfiles/update.sh; rustup update";
             "alpvm" = "sudo systemd-nspawn -x -D ~/Files/Containers/alpine";
-            "gamingcont" = "sudo systemd-nspawn -x -D ~/Files/Containers/arch"; # TODO
+            "gamingcont" = "sudo systemd-nspawn -D ~/Files/Containers/gaming"; # TODO
         };
         initExtra = ''
 export PS1="\[$(tput bold)\]\[\033[38;5;0m\]\[\033[48;5;11m\]\u\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;255m\]\W\[$(tput sgr0)\]\[\033[38;5;11m\]>\[$(tput sgr0)\] \[$(tput sgr0)\]"
